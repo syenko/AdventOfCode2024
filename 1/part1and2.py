646ffce3d9b92cc47ad622db3149cbd281a7eb56
Sabrina <59457991+syenko@users.noreply.github.com>
2024-12-01 21:51:56 -0800

    Clean up day 1

--- 1/part1and2.py
+++ 1/part1and2.py
@@ -1,36 +1,33 @@
+import collections
+
 path = 'test.txt'
 path = 'input.txt'
 
 file = open(path)
 lines = [x.strip() for x in file]
 
-a = []
-b = []
+left = []
+right = []
 for line in lines:
-	vala, valb = line.split("   ")
-	a.append(int(vala))
-	b.append(int(valb))
+	left_val, right_val = line.split()
+	left.append(int(left_val))
+	right.append(int(right_val))
 
-a.sort()
-b.sort()
+left.sort()
+right.sort()
 
 # part 1
 ans = []
-for i in range(len(a)):
-	ans.append(abs(a[i]-b[i]))
+for i in range(len(left)):
+	ans.append(abs(left[i] - right[i]))
 
-print(sum(ans))
+print(f"part one: {sum(ans)}")
 
 # part 2
-counts = dict()
-for i in b:
-	if i not in counts.keys():
-		counts[i] = 0
-	counts[i] += 1
+counts = collections.Counter(right)
 
 ans = 0
-for i in a:
-	if i in counts.keys():
-		ans += i * counts[i]
+for i in left:
+	ans += i * counts[i]
 
-print(ans)
+print(f"part two: {ans}")
