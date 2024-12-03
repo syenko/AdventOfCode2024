d64623bff435890cdb99dc74b39279d660193a20
Sabrina <59457991+syenko@users.noreply.github.com>
2024-12-02 21:16:48 -0800

    Clean up day 3 slightly

--- 3/part2.py
+++ 3/part2.py
@@ -8,10 +8,13 @@ ans = 0
 enabled = True
 for line in lines:
     for match in re.finditer("(do\\(\\))|(don't\\(\\))|(mul\\((\\d+),(\\d+)\\))", line):
+        # do
         if match.group(1):
             enabled = True
+        # do not
         elif match.group(2):
             enabled = False
+        # multiply
         elif match.group(3):
             if enabled:
                 ans += int(match.group(4)) * int(match.group(5))
