60e0d24472e95daab4396adb8b5fd7a23aeb067d
Sabrina <59457991+syenko@users.noreply.github.com>
2024-12-06 21:24:18 -0800

    Clean up day 7 code

--- 7/part1.py
+++ 7/part1.py
@@ -8,23 +8,33 @@ lines = [x.strip() for x in file]
 
 nums = [1, 2, 3]
 
-def helper(cur, res, i):
+'''
+recursive helper function to see if a given equation
+can be solved by inserting + or * operators
+
+res is the final answer, cur is the sum so far, i
+is the index in nums (global variable)
+'''
+def can_solve_helper(cur, res, i):
+    # if we've reached the end -> return whether we got the right value
     if i == len(nums):
         return cur == res
+    # if it's too big -> exist immediately
     if cur > res:
         return False
     else:
-        return helper(cur * nums[i], res, i + 1) or helper(cur + nums[i], res, i + 1)
+        # try both options + / *
+        return can_solve_helper(cur * nums[i], res, i + 1) or can_solve_helper(cur + nums[i], res, i + 1)
 
 
-def is_true(line):
+def get_calibration_value(line):
     res, _nums = line.split(": ")
     res = int(res)
     nums.clear()
     nums.extend([int(x) for x in _nums.strip().split()])
 
-    return res if helper(nums[0], res, 1) else 0
+    return res if can_solve_helper(nums[0], res, 1) else 0
 
-ans = [is_true(line) for line in lines]
+ans = [get_calibration_value(line) for line in lines]
 
 print(sum(ans))
