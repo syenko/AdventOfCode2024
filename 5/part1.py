c3678658fc21ad0d34d3c01a44e29eab8f4680fd
Sabrina <59457991+syenko@users.noreply.github.com>
2024-12-16 00:22:39 -0800

    Add day 5

---
+++ 5/part1.py
@@ -0,0 +1,36 @@
+import re
+import collections
+from collections import defaultdict
+
+file = open('input.txt')
+# file = open('test.txt')
+
+lines = [x.strip() for x in file]
+
+rules = defaultdict(list)
+
+i = 0
+line = lines[i]
+while line != "":
+    before, after = (int(x) for x in line.split("|"))
+    rules[after].append(before)
+    i += 1
+    line = lines[i]
+
+def passes_rules(nums):
+    nums_to_pages = {num: i for i, num in enumerate(nums)}
+    for num in nums:
+        position = nums_to_pages[num]
+        for rule in rules[num]:
+            if rule in nums_to_pages and nums_to_pages[rule] > position:
+                return False
+    return True
+
+ans = 0
+for line in lines[i + 1:]:
+    nums = [int(x) for x in line.split(",")]
+
+    if passes_rules(nums):
+       ans += nums[len(nums) // 2]
+
+print(ans)
---
+++ 5/part2.py
@@ -0,0 +1,46 @@
+import re
+import collections
+from collections import defaultdict
+from functools import cmp_to_key
+
+file = open('input.txt')
+# file = open('test.txt')
+
+lines = [x.strip() for x in file]
+
+rules = defaultdict(list)
+
+i = 0
+line = lines[i]
+while line != "":
+    before, after = (int(x) for x in line.split("|"))
+    rules[after].append(before)
+    i += 1
+    line = lines[i]
+
+def passes_rules(nums):
+    nums_to_pages = {num: i for i, num in enumerate(nums)}
+    for num in nums:
+        position = nums_to_pages[num]
+        for rule in rules[num]:
+            if rule in nums_to_pages and nums_to_pages[rule] > position:
+                return False
+    return True
+
+def compare(a, b):
+    if a in rules[b]:
+        return -1
+    elif b in rules[a]:
+        return 1
+    else:
+        return 0
+
+ans = 0
+for line in lines[i + 1:]:
+    nums = [int(x) for x in line.split(",")]
+
+    if not passes_rules(nums):
+        nums = sorted(nums, key=cmp_to_key(compare))
+        ans += nums[len(nums) // 2]
+
+print(ans)
