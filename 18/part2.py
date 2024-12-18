7b9a72e7caa1d39931dbea59b59063cce31c5d1b
Sabrina <59457991+syenko@users.noreply.github.com>
2024-12-17 21:35:42 -0800

    Add day 18

---
+++ 18/part2.py
@@ -0,0 +1,84 @@
+import re
+import collections
+from collections import namedtuple
+from dataclasses import dataclass
+
+file = open('input.txt')
+SIZE = 70
+CAP = 1024
+# file = open('test.txt')
+# SIZE = 6
+# CAP = 12
+
+lines = [x.strip() for x in file]
+
+Coord = namedtuple('Coord', ['x', 'y'])
+
+@dataclass()
+class Node:
+    loc: Coord
+    steps: int = 0
+
+def in_bounds(loc: Coord):
+    if loc.x < 0 or loc.x > SIZE or loc.y < 0 or loc.y > SIZE:
+        return False
+    return True
+
+
+m = [[False for x in range(SIZE + 1)] for _ in range(SIZE + 1)]
+coords = []
+for i, line in enumerate(lines):
+    x, y = [int(x) for x in line.split(",")]
+    coords.append(Coord(x, y))
+    if i < CAP:
+        m[y][x] = True
+m_viz = [["#" if x else "." for x in line] for line in m]
+
+def display(m):
+    for line in m:
+        print("".join(line))
+
+display(m_viz)
+
+def bfs() -> bool:
+    q = collections.deque()
+    start = Coord(0, 0)
+    end = Coord(SIZE, SIZE)
+    q.append((start, 0))
+    visited = {start}
+    while len(q) != 0:
+        node, num_steps = q.popleft()
+        # print(node, num_steps)
+        # found end!
+        if node == end:
+            print(node, num_steps)
+            return True
+
+        neighbors = [
+            Coord(node.x - 1, node.y),
+            Coord(node.x + 1, node.y),
+            Coord(node.x, node.y + 1),
+            Coord(node.x, node.y - 1)
+        ]
+
+        for loc in neighbors:
+            # print(loc, m[loc.x][loc.y])
+            # is wall or out of bounds
+            if not in_bounds(loc) or m[loc.y][loc.x]:
+                continue
+            # update neighbors
+            if loc not in visited:
+                # print(loc, len(q))
+                visited.add(loc)
+                q.append((loc, num_steps + 1))
+    return False
+
+i = CAP
+can_bfs = True
+while can_bfs:
+    m[coords[i].y][coords[i].x] = True
+    can_bfs = bfs()
+    i += 1
+
+print(i)
+print(coords[i - 1])
