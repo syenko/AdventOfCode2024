c46aa3780a324faf187bedff2f7de4400c4f4d27
Sabrina <59457991+syenko@users.noreply.github.com>
2024-12-19 23:18:48 -0800

    Clean up day 20

--- 20/part1.py
+++ 20/part1.py
@@ -9,21 +9,17 @@ from typing import Any, Self, Tuple, List, Set
 file = open('input.txt')
 # file = open('test.txt')
 
-Coord = collections.namedtuple('Coord', ['x', 'y'])
-
 CHEAT_SECS = 2
 
 lines = [x.strip() for x in file]
 
 m = [[x == "#" for x in line] for line in lines]
-# map for visualization purposes
-m_visual = [[v for v in x] for x in lines]
 
 nodes = dict()
 WIDTH = len(lines[0])
 HEIGHT = len(m)
-TURN_TIME = 1000
-MOVE_TIME = 1
+
+Coord = collections.namedtuple('Coord', ['x', 'y'])
 
 @dataclass(unsafe_hash=True, order=True)
 class Node:
@@ -49,7 +45,7 @@ for y, line in enumerate(lines):
         nodes[Coord(x, y)] = Node(loc = Coord(x, y))
 
 # Dijkstra's Algorithm
-def dijkstra_norm(m, start, end):
+def dijkstra(m: List[List[bool]], start: Coord, end: Coord) -> List[Node]:
     q = queue.PriorityQueue()
     q.put(Node(start, 0, dist=0))
     visited = {(0, start)}
@@ -86,11 +82,21 @@ def dijkstra_norm(m, start, end):
                 neighbor_node.prev = node
                 q.put(neighbor_node)
 
-best_path = dijkstra_norm(m, start, end)
+best_path = dijkstra(m, start, end)
 best_dist = len(best_path)
 print(best_dist - 1)
 
-def manhatten_distance(start, end):
+'''
+Solution Idea: 
+Save time by jumping ahead on the best path.
+To find when you can save time, look at all pairs of 
+points on the best path and calculate if you can get
+from one to another without walls (manhattan distance).
+
+If you can, see if that actually saves time by checking
+how much you jump ahead by.
+'''
+def manhattan_distance(start, end):
     return abs(end.x - start.x) + abs(end.y - start.y)
 
 dist_saved = []
@@ -98,11 +104,12 @@ dist_saved = []
 for i, start in enumerate(best_path):
     for j in range(i + 1, len(best_path)):
         end = best_path[j]
-        # too far
-        md = manhatten_distance(start.loc, end.loc)
+        md = manhattan_distance(start.loc, end.loc)
         if md > CHEAT_SECS or (j-i) - md < 100:
             continue
         dist_saved.append((j-i) - md)
+    if i % 100 == 0:
+        print(".", end="")
 
-print(dist_saved)
+print()
 print(len(dist_saved))
