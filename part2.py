5ec486b5ae7388b5e3255a3386d2ebc50d7b71f3
Sabrina <59457991+syenko@users.noreply.github.com>
2024-12-20 13:54:33 -0800

    Update template

--- part2.py
+++ part2.py
@@ -1,5 +1,14 @@
 import re
 import collections
+import itertools
+
+class Coord(collections.namedtuple('Coord', ['x', 'y'])):
+    def __add__(self, other):
+        return Coord(**{field: getattr(self, field) + getattr(other, field)
+                        for field in self._fields})
+    def __sub__(self, other):
+        return Coord(**{field: getattr(self, field) - getattr(other, field)
+                        for field in self._fields})
 
 file = open('input.txt')
 file = open('test.txt')
