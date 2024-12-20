ed7a7fdaddcf2fb5066eff566aa81974b88de089
Sabrina <59457991+syenko@users.noreply.github.com>
2024-12-20 13:58:16 -0800

    Add day 8

---
+++ 8/part2.py
@@ -0,0 +1,48 @@
+import itertools
+import re
+import collections
+
+class Coord(collections.namedtuple('Coord', ['x', 'y'])):
+    def __add__(self, other):
+        return Coord(**{field: getattr(self, field) + getattr(other, field)
+                        for field in self._fields})
+    def __sub__(self, other):
+        return Coord(**{field: getattr(self, field) - getattr(other, field)
+                        for field in self._fields})
+
+file = open('input.txt')
+# file = open('test.txt')
+
+lines = [x.strip() for x in file]
+WIDTH = len(lines[0])
+HEIGHT = len(lines)
+
+def in_bounds(loc: Coord):
+    if loc.x < 0 or loc.x >= WIDTH or loc.y < 0 or loc.y >= HEIGHT:
+        return False
+    return True
+
+antennas = collections.defaultdict(list)
+
+for y, line in enumerate(lines):
+    for x, char in enumerate(line):
+        if char != ".":
+            antennas[char].append(Coord(x, y))
+
+locs = set()
+
+for key, val in antennas.items():
+    for a, b in itertools.combinations(val, 2):
+        locs.add(a)
+        locs.add(b)
+        diff = b - a
+        p1 = a - diff
+        while in_bounds(p1):
+            locs.add(p1)
+            p1 = p1 - diff
+        p2 = b + diff
+        while in_bounds(p2):
+            locs.add(p2)
+            p2 = p2 + diff
+
+print(len(locs))
