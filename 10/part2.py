f117712294be6802986c4f8aa2e50e552e201498
Sabrina <59457991+syenko@users.noreply.github.com>
2024-12-22 17:04:50 -0800

    Add day 10 part 2

--- 10/part2.py
+++ 10/part2.py
@@ -1,6 +1,7 @@
 import re
 import collections
 import itertools
+from typing import List
 
 class Coord(collections.namedtuple('Coord', ['x', 'y'])):
     def __add__(self, other):
@@ -11,6 +12,39 @@ class Coord(collections.namedtuple('Coord', ['x', 'y'])):
                         for field in self._fields})
 
 file = open('input.txt')
-file = open('test.txt')
+# file = open('test.txt')
 
 lines = [x.strip() for x in file]
+m = [[int(val) for val in line] for line in lines]
+WIDTH = len(m[0])
+HEIGHT = len(m)
+
+def in_bounds(loc: Coord):
+    if loc.x < 0 or loc.x >= WIDTH or loc.y < 0 or loc.y >= HEIGHT:
+        return False
+    return True
+
+def nines_count(coord: Coord, m: List[List[int]]) -> int:
+    height = m[coord.y][coord.x]
+    if height == 9:
+        return 1
+    else:
+        neighbors = [
+            Coord(coord.x - 1, coord.y),
+            Coord(coord.x + 1, coord.y),
+            Coord(coord.x, coord.y + 1),
+            Coord(coord.x, coord.y - 1)
+        ]
+        num_nines = 0
+        for n in neighbors:
+            if in_bounds(n) and m[n.y][n.x] == height + 1:
+                num_nines += nines_count(n, m)
+        return num_nines
+
+trail_vals = []
+for y, line in enumerate(m):
+    for x, height in enumerate(line):
+        if height == 0:
+            trail_vals.append(nines_count(Coord(x, y), m))
+
+print(sum(trail_vals))
